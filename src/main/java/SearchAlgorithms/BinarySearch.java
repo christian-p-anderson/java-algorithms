b51de3d52a439b80dbb7da0873e771bee573e08d
nicolepompei <nicole.lynn.pompei@gmail.com>
2020-12-15 23:34:30 -0500

    binary search recusrive and iterative and bubble sort

--- src/main/java/SearchAlgorithms/BinarySearch.java
+++ src/main/java/SearchAlgorithms/BinarySearch.java
@@ -1,4 +1,54 @@
 package SearchAlgorithms;
 
 public class BinarySearch {
+
+    /**
+     * Recursive Binary Search (if target is not found, return -1, but ask interviewer what
+     * they would like to be returned)
+     */
+
+    public int binarySearchRecursive(int[] array, int target){
+        return binarySearchHelperRecursive(array,target, 0, array.length - 1);
+    }
+
+    public int binarySearchHelperRecursive(int[] array, int target, int left, int right){
+        if( left > right){
+            return -1;
+        }
+        int middle = Math.round((left + right)/2);
+        int potentialMatch = array[middle];
+
+        if(potentialMatch == target){
+            return middle;
+        } else if(potentialMatch > target ){
+            return binarySearchHelperRecursive(array, target, left, middle - 1);
+        } else{
+            return binarySearchHelperRecursive(array, target, middle + 1, right);
+        }
+    }
+
+
+    /**
+     * Iterative Binary Search (helper fn not needed)
+     */
+
+    public int iterativeBinarySearch(int[] array, int target){
+        int left = 0;
+        int right = array.length -1;
+
+        while(left <= right){
+            int middle = Math.round((left + right) / 2);
+            int potentalMatch = array[middle];
+
+            if(potentalMatch == target){
+                return middle;
+            } else if(potentalMatch > target){
+                right = middle - 1;
+            } else{
+                left = middle + 1;
+            }
+        }
+        return -1;
+    }
+
 }
--- src/main/java/SearchAlgorithms/BubbleSort.java
+++ src/main/java/SearchAlgorithms/BubbleSort.java
@@ -1,4 +1,18 @@
 package SearchAlgorithms;
 
 public class BubbleSort {
+
+    public int[] bubbleSort(int[] array){
+        int temp = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+            if(array[j] > array[j+1]){
+                temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+        }
+        return array;
+    }
 }
