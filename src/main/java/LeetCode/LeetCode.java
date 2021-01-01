9614242ce2cc5a030188622df48f5098ff76714d
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-01 11:57:33 -0500

    added leetcode fizzBuzz problem solution with for loop and variables for diff string results

--- src/main/java/LeetCode/LeetCode.java
+++ src/main/java/LeetCode/LeetCode.java
@@ -1,5 +1,8 @@
 package LeetCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeetCode {
 
     /**
@@ -36,5 +39,57 @@ public class LeetCode {
             start++;
             end--;
         }
+    }
+
+    /**
+     * Write a program that outputs the string representation of numbers from 1 to n.
+     *
+     * But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”.
+     * For numbers which are multiples of both three and five output “FizzBuzz”.
+     *
+     * Example:
+     * n = 15,
+     *
+     * Return:
+     * [
+     *     "1",
+     *     "2",
+     *     "Fizz",
+     *     "4",
+     *     "Buzz",
+     *     "Fizz",
+     *     "7",
+     *     "8",
+     *     "Fizz",
+     *     "Buzz",
+     *     "11",
+     *     "Fizz",
+     *     "13",
+     *     "14",
+     *     "FizzBuzz"
+     * ]
+     */
+
+    public List<String> fizzBuzz(int n){
+
+        List<String> result = new ArrayList<>();
+
+        for (int i = 1; i <= n ; i++) {
+            String fizz = "Fizz";
+            String buzz = "Buzz";
+            String fizzBuzz = "FizzBuzz";
+            String num = String.valueOf(i);
+
+            if(n % 3 == 0 && n % 15 == 0){
+                result.add(fizzBuzz);
+            } else if( n % 3 == 0){
+                result.add(fizz);
+            } else if (n % 5 == 0){
+                result.add(buzz);
+            } else{
+                result.add(num);
+            }
+        }
+        return result;
     }
 }
