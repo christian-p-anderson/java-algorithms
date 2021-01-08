0e1a37dd591a15ee7ab00eda9cf86645a668b831
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-08 15:09:06 -0500

    string array drills

--- src/main/java/Drills/Array.java
+++ src/main/java/Drills/Array.java
@@ -1,4 +1,31 @@
 package Drills;
 
 public class Array {
+
+    /**
+    *
+    *@param array an array of String objects
+     * @return the first element in the array
+     */
+    public String getFirstElement(String[] array){
+        return array[0];
+    }
+
+    /**
+     * @param stringArray an array of String objects
+     * @return the second element in the array
+     */
+    public String getSecondElement(String[] stringArray){
+        return stringArray[1];
+    }
+
+    /**
+     * @param stringArray an array of String objects
+     * @return the last element in the array
+     */
+    public String getLastElement(String[] stringArray){
+        return stringArray[stringArray.length - 1];
+    }
+
+
 }
