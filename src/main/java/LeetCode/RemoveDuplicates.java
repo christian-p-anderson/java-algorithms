94205a6297f71e6804f96b853caee8d789f387d4
Christian Anderson <christian.p.anderson1@gmail.com>
2021-01-21 18:48:22 -0500

    completed RemoveDuplicates

---
+++ src/main/java/LeetCode/RemoveDuplicates.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+public class RemoveDuplicates {
+
+    /**
+     *
+     * Remove duplicates from Sorted Array
+     *
+     * Given a sorted array, the task is to remove the duplicate elements from the array.
+     *
+     */
+
+    static int removeDuplicates(int[] arr) {
+        int n = arr.length;
+
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        int j = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                arr[j++] = arr[i];
+            }
+        }
+        arr[j++] = arr[n - 1];
+        return j;
+
+    }
+
+}
