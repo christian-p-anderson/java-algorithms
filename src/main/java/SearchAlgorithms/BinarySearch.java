30505765c2e5d751dd458a413c27d6bbaaf7a22f
Christian Anderson <christian.p.anderson1@gmail.com>
2021-01-25 10:05:03 -0500

    completed BinarySearch algorithm

--- src/main/java/SearchAlgorithms/BinarySearch.java
+++ src/main/java/SearchAlgorithms/BinarySearch.java
@@ -1,4 +1,27 @@
 package SearchAlgorithms;
 
 public class BinarySearch {
+
+    /**
+     *
+     * This is the iterative approach
+     */
+
+    public static int binarySearch (int[] arr, int x) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int middle = left + (right - 1) / 2;
+
+            if (arr[middle] == x) {
+                return middle;
+            } else if (arr[middle] < x) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+        return -1;
+    }
 }
