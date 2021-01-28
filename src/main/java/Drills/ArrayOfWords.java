ee48779cbb225a0d0ae80e0e0201f7d4146c21ac
Christian Anderson <christian.p.anderson1@gmail.com>
2021-01-28 17:49:31 -0500

    added algo to create an array of strings from a sentence

--- src/main/java/Drills/ArrayOfWords.java
+++ src/main/java/Drills/ArrayOfWords.java
@@ -1,2 +1,20 @@
-package Drills;public class ArrayOfWords {
+package Drills;
+
+import java.util.Arrays;
+
+public class ArrayOfWords {
+
+    /**
+     * take a sentence and return an array of words
+     */
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(returnArrayOfWords("I am hungry")));
+    }
+
+
+    public static String[] returnArrayOfWords(String str) {
+        return str.split(" ");
+    }
+
 }
